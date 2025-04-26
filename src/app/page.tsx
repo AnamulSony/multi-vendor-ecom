6d1d97cb713ffcff2b18c0886af6375b8aad5b86
unknown <anamul.ru.cse@gmail.com>
2025-04-26 16:12:39 +0600

    set the css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,35 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
+
+
 
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-500">Hello world</p>
+    <div className="p-4">
+        <div className="flex flex-col gap-y-4">
+          <div>
+            <Button variant="elevated">
+            I am button
+            </Button>
+          </div>
+          <div>
+            <Input placeholder="I am an input"></Input>
+          </div>
+          <div>
+            <Progress value={50}/>
+          </div>
+          <div>
+            <Textarea  placeholder="I am a textarea"/>
+          </div>
+          <div>
+            <Checkbox />
+          </div>
+        </div>
     </div>
+    
   );
 }
